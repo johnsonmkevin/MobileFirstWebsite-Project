1b30381bdb049c4bac9263eb251441628fcbc720
johnsonmkevin <harlemworld1985@gmail.com>
2022-10-05 19:32:20 +0200

    clickmenu

--- script.js
+++ script.js
@@ -1,121 +1,89 @@
 function kempPic1() {
   let seanKemp = document.getElementById("seanKempPic");
-  seanKemp.classList.toggle("SeanKempDunk");
   seanKemp.classList.toggle("SeanKempDunk2");
 }
 
 let seanKemp = document.getElementById("green-left");
 seanKemp.addEventListener("click", kempPic1);
 
-
 function kempPic2() {
   let seanKemp2 = document.getElementById("seanKempPic");
-  seanKemp2.classList.toggle("SeanKempDunk");
   seanKemp2.classList.toggle("SeanKempDunk2");
 }
 
 let seanKemp2 = document.getElementById("green-right");
 seanKemp2.addEventListener("click", kempPic2);
 
-
 function jordan1() {
   let jordanpic1 = document.getElementById("MichaelJordanPic");
-  jordanpic1.classList.toggle("MichaelJordanStance");
   jordanpic1.classList.toggle("MichaelJordanStance2");
-
 }
 
 let jordanpic1 = document.getElementById("red-left");
 jordanpic1.addEventListener("click", jordan1);
 
-
-
 function jordan2() {
   let jordanpic2 = document.getElementById("MichaelJordanPic");
-  jordanpic2.classList.toggle("MichaelJordanStance");
   jordanpic2.classList.toggle("MichaelJordanStance2");
-
 }
 
 let jordanpic2 = document.getElementById("red-right");
 jordanpic2.addEventListener("click", jordan1);
 
-
-
-
-
-
-
 function barkley1() {
   let barkleypic1 = document.getElementById("CharlesBarkleyPic");
-  barkleypic1.classList.toggle("SirCharlesBarkley");
   barkleypic1.classList.toggle("SirCharlesBarkley2");
-
 }
 
 let barkleypic1 = document.getElementById("orange-left");
 barkleypic1.addEventListener("click", barkley1);
 
-
-
 function barkley2() {
   let barkleypic2 = document.getElementById("CharlesBarkleyPic");
-  barkleypic2.classList.toggle("SirCharlesBarkley");
   barkleypic2.classList.toggle("SirCharlesBarkley2");
-
 }
 
 let barkleypic2 = document.getElementById("orange-right");
 barkleypic2.addEventListener("click", barkley2);
 
-
-
-
-
-
 function penny1() {
   let pennyPic1 = document.getElementById("PennyHardway");
-  pennyPic1.classList.toggle("Penny1");
   pennyPic1.classList.toggle("Penny2");
-
 }
 
 let pennyPic1 = document.getElementById("blue-left");
 pennyPic1.addEventListener("click", penny1);
 
-
-
 function penny2() {
   let pennyPic2 = document.getElementById("PennyHardway");
-  pennyPic2.classList.toggle("Penny1");
   pennyPic2.classList.toggle("Penny2");
-
 }
 
 let pennyPic2 = document.getElementById("blue-right");
 pennyPic2.addEventListener("click", penny2);
 
-
-
 function kobe1() {
   let kobePic1 = document.getElementById("KobeBryantPic");
-  kobePic1.classList.toggle("KobeBryant-1");
   kobePic1.classList.toggle("KobeBryant-2");
-
 }
 
 let kobePic1 = document.getElementById("purple-left");
 kobePic1.addEventListener("click", kobe1);
 
-
-
 function kobe2() {
   let kobePic2 = document.getElementById("KobeBryantPic");
-  kobePic2.classList.toggle("KobeBryant-1");
   kobePic2.classList.toggle("KobeBryant-2");
-
 }
 
 let kobePic2 = document.getElementById("purple-right");
 kobePic2.addEventListener("click", kobe2);
 
+
+function menuMobile() {
+  let navbarList = document.getElementById("navbarList");
+  navbarList.classList.toggle("navbar-hide");
+}
+
+let kevinButton = document.getElementById("buttonChange");
+
+kevinButton.addEventListener("click", menuMobile);
